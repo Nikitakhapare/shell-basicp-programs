c367452c032590c8aa50b133fa17452622d139cb
Nikita <nikitakhapare53@gmail.com>
2022-04-28 15:51:38 +0530

    [nikita]Add:Shell script program to get days of week as per user input using if-elif statement

---
+++ conditional-statement/days.sh
@@ -0,0 +1,29 @@
+read -p "Enter a Number: " n
+
+if [ $n == 1 ]
+then 
+ echo "its Monday"
+
+elif [ $n == 2 ]
+then
+ echo "its tuesday"
+
+elif [ $n == 3 ]
+then
+ echo "its wednesday"
+
+elif [ $n == 4 ]
+then
+ echo "its thusday"
+
+elif [ $n == 5 ]
+then
+ echo "its saturday"
+
+elif [ $n == 6 ]
+then
+ echo "its sunday"
+
+fi
+
+
