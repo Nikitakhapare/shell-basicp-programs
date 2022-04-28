dd87f8c391d4b8e8cf263b8035d9c2518efc7c62
Nikita <nikitakhapare53@gmail.com>
2022-04-28 15:48:58 +0530

    [nikita]Add:Shell script program to check to no are equal or greater using if statement

---
+++ conditional-statement/Check-Two-No.sh
@@ -0,0 +1,16 @@
+a=2
+b=7
+if [ $a -eq $b ]
+then
+  echo "The variable 'a' is equal to 'b'."
+else
+  echo "The variable 'b' is not eaual 'a'."
+fi
+
+if [ $a -gt $b ]
+then 
+ echo "The variable a is greater than b"
+else
+ echo "The variable b is greater"
+fi
+
