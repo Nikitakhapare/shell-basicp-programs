e06bb33d9ee5340b7a6aa9b21a70fbb759608497
Nikita <nikitakhapare53@gmail.com>
2022-04-28 22:48:59 +0530

    [nikita]Add: Shell script to program to create simple function

---
+++ Functional/sample.sh
@@ -0,0 +1,19 @@
+#fuction created uysing function keyword
+function first(){
+  echo  "these is my first function"
+}
+
+function parameter-calling(){
+ echo $1 $2
+}
+
+# Function created without using function keyword
+ 
+
+quit(){
+ exit
+}
+
+first
+parameter-calling hello niki how are you
+quit
